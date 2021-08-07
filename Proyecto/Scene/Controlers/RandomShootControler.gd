e804a63db3decb789a820a887cdf214454929ff2
LEANDRO <leandroezequiel1999@gmail.com>
2021-08-07 12:39:26 -0300

    change random shoot behavior

--- Proyecto/Scene/Controlers/RandomShootControler.gd
+++ Proyecto/Scene/Controlers/RandomShootControler.gd
@@ -6,8 +6,14 @@ export var max_time : float = 2.0
 
 onready var timer := $Timer
 
+func _ready() -> void:
+	_set_random_time()
+
 func _on_Timer_timeout():
 	tank.shoot()
+	_set_random_time()
+
+func _set_random_time():
 	rng.randomize()
 	timer.wait_time = rng.randf_range(min_time, max_time)
 	timer.start()
